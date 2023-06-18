a20d2add30e4d434b02b6a9a08cee0f6d7cfe1f2
essyQuinn <esthergithinji49@gmail.com>
2023-06-18 11:04:53 +0300

    about section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import TopSection from './Components/TopSection';
+import About from './Components/About';
+
 
 function App() {
   return (
     <div className="App">
       <TopSection/>
+      <About/>
     </div>
   );
 }
