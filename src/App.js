847cc095a0d120ded4b01748c7a442436d685a07
essyQuinn <esthergithinji49@gmail.com>
2023-06-18 12:40:51 +0300

    new files and changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import TopSection from './Components/TopSection';
-import About from './Components/About';
+import About from './Components/AboutSection';
 import Contact from './Components/Contact';
+import Footer from './Components/Footer';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <TopSection/>
       <About/>
       <Contact/>
+      <Footer/>
     </div>
   );
 }
